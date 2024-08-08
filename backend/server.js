30621cf05e528dcbdf70e8f0a86240449ab8cc92
Nelson Paing <25paingn@isyedu.org>
2024-08-09 00:21:08 +0630

    dep 6a

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: false, // for local testing
+      secure: true, // for local testing
       sameSite: "None", // or "strict" for local
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     },
