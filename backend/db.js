9571fd825b9a6360fad9e5900fee44fef180b5fc
Nelson Paing <25paingn@isyedu.org>
2024-08-08 23:28:10 +0630

    dep 3a

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import mysql2 from "mysql2";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const db = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: "mysql",
+    dialectModule: mysql2,
   }
 );
 
