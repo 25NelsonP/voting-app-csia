943172a0138ac3b9b203ebc6b99c187656498bab
Nelson Paing <25paingn@isyedu.org>
2024-08-08 23:41:22 +0630

    dep 4a

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: false, // for local testing
-      sameSite: "lax", // or "strict" for local
+      secure: true, // for local testing
+      sameSite: "None", // or "strict" for local
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     },
   })
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
+  const [loading, setLoading] = useState(false);
   const API_URL = process.env.REACT_APP_API_URL;
 
   const handleLogin = () => {
+    setLoading(true);
     window.location.href = `${API_URL}/auth/google`;
   };
 
@@ -19,13 +21,16 @@ const Login = () => {
         />
         <button
           onClick={handleLogin}
-          className="bg-blue-600 text-white border font-medium flex items-center justify-center hover:bg-blue-700 transition duration-200"
+          className={`bg-blue-600 text-white border font-medium flex items-center justify-center hover:bg-blue-700 transition duration-200 ${
+            loading ? "opacity-50 cursor-not-allowed" : ""
+          }`}
+          disabled={loading}
         >
           <div className="bg-white p-3">
             <FcGoogle size={20} />
           </div>
           <span className="flex-1 text-center py-2 px-4">
-            Continue with Google
+            {loading ? "Redirecting..." : "Continue with Google"}
           </span>
         </button>
       </div>
