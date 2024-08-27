f4c8ab7a7df44200cd0fd09a13a036ede8147f0d
Nelson Paing <25paingn@isyedu.org>
2024-08-27 21:07:16 +0630

    db connection update

--- backend/db.js
+++ backend/db.js
@@ -1,22 +1,35 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
 import mysql2 from "mysql2";
 
-dotenv.config();
+// Load environment variables from a .env file if not in production
+// for future deployments not vercel
+// if (process.env.NODE_ENV !== 'production') {
+//   dotenv.config({
+//       path: process.env.NODE_ENV === 'staging' ? '.env.staging' : '.env.local'
+//   });
+// }
 
+// Define the Sequelize configuration
 const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_NAME, // The database name
+  process.env.DB_USER, // The database user
+  process.env.DB_PASS, // The password for the database user
   {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    dialectModule: mysql2,
+    host: process.env.DB_HOST, // The database host
+    port: process.env.DB_PORT, // The port to connect to
+    dialect: "mysql", // The type of database
+    dialectModule: mysql2, // The mysql2 module for MySQL connection
+    dialectOptions:
+      process.env.NODE_ENV === "production"
+        ? {
+            ssl: {
+              require: true, // Enforce SSL connection
+              rejectUnauthorized: false, // Allows self-signed certificates
+              ca: process.env.CA_CERT, // Path to the CA certificate
+            },
+          }
+        : {}, // Empty object for non-production (no SSL configuration)
   }
 );
-
-db.authenticate().catch((err) => {
-  console.error("Unable to connect to the database:", err);
-});
 
 export default db;
